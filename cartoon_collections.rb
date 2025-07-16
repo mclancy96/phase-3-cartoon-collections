64fb467836cfba0932a08dfaee771f85bcb59bcc
Mike <mclancy96@gmail.com>
2025-07-15 21:45:19 -0400

    Implement cartoon methods

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each.with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteers
+  planeteers.map { |planeteer| "#{planeteer.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  !!calls.find { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese options
   cheese_types = ["cheddar", "gouda", "camembert"]
+  options.find { |option| cheese_types.include? option} || nil
 end
